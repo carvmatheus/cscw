6def75024aa5cfa7e09cd8c1f1b67084aa6ba065
carvmatheus <mathcarvcard@gmail.com>
2023-10-01 07:56:41 -0300

    teste das cscw

---
+++ constant.py
@@ -0,0 +1,3 @@
+# Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
+# and rename this file to constants.py.
+APIKEY = "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"
